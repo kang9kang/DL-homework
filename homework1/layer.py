1778eb4ee6e2ec9fdafe3eca6dab4988917ffbc8
kang9kang <13589130504@163.com>
2023-10-07 19:27:46 +0800

    move dir

--- homework1/layer.py
+++ homework1/layer.py
@@ -56,9 +56,9 @@ class MyBatchNorm(MyLayer):
             self.var = X.var(axis=0)
             self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * self.mean
             self.running_var = self.momentum * self.running_var + (1 - self.momentum) * self.var
-            self.X_hat = (X - self.mean) / np.sqrt(self.var + self.eps)
+            self.X_hat = ((X - self.mean) / np.sqrt(self.var + self.eps)).view(np.ndarray)
         else:
-            self.X_hat = (X - self.running_mean) / np.sqrt(self.running_var + self.eps)
+            self.X_hat = ((X - self.running_mean) / np.sqrt(self.running_var + self.eps)).view(np.ndarray)
         return self.X_hat * self.gamma + self.beta
     
     def backward(self, eta: np.ndarray) -> np.ndarray:
